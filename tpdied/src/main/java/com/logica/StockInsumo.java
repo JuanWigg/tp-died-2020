ef6f3c322e9123a8d2261501c913097867317d63
josesei <joseignaciocarbone@gmail.com>
2020-08-06 19:13:40 -0300

    Refactor clases jose

--- tpdied/src/main/java/com/logica/StockInsumo.java
+++ tpdied/src/main/java/com/logica/StockInsumo.java
@@ -2,18 +2,36 @@ package com.logica;
 
 public class StockInsumo {
 	
-	private Magnitud cantidad;
+	private double cantidad;
+	private double puntoDePedido;
+	private Insumo insumo;
 
-	public Magnitud getCantidad() {
+	public double getCantidad() {
 		return cantidad;
 	}
 
-	public void setCantidad(Magnitud cantidad) {
+	public void setCantidad(double cantidad) {
 		this.cantidad = cantidad;
 	}
 
 	public StockInsumo() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public double getPuntoDePedido() {
+		return puntoDePedido;
+	}
+
+	public void setPuntoDePedido(double puntoDePedido) {
+		this.puntoDePedido = puntoDePedido;
+	}
+
+	public Insumo getInsumo() {
+		return insumo;
+	}
+
+	public void setInsumo(Insumo insumo) {
+		this.insumo = insumo;
 	}
 
 }
